b63c694b4a1d072e33294938c2003877432d0e34
Robert MacLean <robert@sadev.co.za>
2014-07-08 09:45:51 +0200

    Added an integrer to brush converter

---
+++ converters/IntToBrush.cs
@@ -0,0 +1,26 @@
+﻿namespace appfactory.core.converters
+{
+    using System;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Media;
+
+    public class IntToBrush : IValueConverter
+    {
+        public Brush EqualsBrush { get; set; }
+
+        public int Match { get; set; }
+
+        public Brush NoEqualBrush { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var result = (int)value == Match;
+            return result ? EqualsBrush : NoEqualBrush;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, string language)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
