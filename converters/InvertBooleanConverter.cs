45e541f24b6ab5751c0d3a8aeab9a513de3e7c47
Robert MacLean <robert@sadev.co.za>
2014-09-04 11:07:33 +0200

    Added invert bool converter

--- converters/InvertBooleanConverter.cs
+++ converters/InvertBooleanConverter.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="CurrencyToVisibility.cs" company="AppFactory Team">
+// <copyright file="InvertBooleanConverter.cs" company="AppFactory Team">
 //     Copyright AppFactory Team. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.
 // </copyright>
 //-----------------------------------------------------------------------
@@ -7,23 +7,13 @@
 namespace appfactory.core.converters
 {
     using System;
-    using System.Globalization;
-    using Windows.UI.Xaml;
     using Windows.UI.Xaml.Data;
 
-    public class CurrencyToVisibility : IValueConverter
+    public class InvertBooleanConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            var amountString = ((string)value).ToUpperInvariant().Replace("R", "");
-            var amount = System.Convert.ToDouble(amountString, CultureInfo.InvariantCulture);
-
-            if (amount > 0)
-            {
-                return Visibility.Visible;
-            }
-
-            return Visibility.Collapsed;
+            return !(bool)value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
